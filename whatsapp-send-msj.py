bc155212125d9199dd90cd9f74417468a08a0672
Mihai293 <mihai.axinte23@gmail.com>
2022-10-24 22:12:49 +0300

    hey

--- whatsapp-send-msj.py
+++ whatsapp-send-msj.py
@@ -1,7 +1,3 @@
 import pywhatkit
-
-phone_number = ["+40725015306", "+40724013049"]
-# phone_number = input("Numar telefon: ")
-for n in phone_number:
-    pywhatkit.sendwhatmsg(n, "test", 21, 40,)
-# pywhatkit.sendwhatmsg(phone_number, "test", 21, 35)
+phone_number = input("Numar telefon: ")
+pywhatkit.sendwhatmsg(phone_number, "test", 21, 35)
